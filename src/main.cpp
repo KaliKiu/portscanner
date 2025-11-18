9fa264aaee2533d54176ddf0c23958e286d4b94f
KaliKiu <palablink@gmail.com>
2025-11-18 18:13:17 +0100

    fixing unique_ptr issues, constructing the IP hdr function

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,16 @@
 #include <arpa/inet.h> 
 #include <stdio.h>
 #include "include/json.hpp"
+#include <memory>
 
-#pragma comment(lib, "ws2_32.lib") // Link Winsock library
-
-const char* IPFILE = "data/ip_addr.json";
-
-class Packet {
-const char* IP_CONFIG_FILE = "data/tcpsyn_header.json";
-
-typedef struct __attribute__((packed)){
+class IP{
+public:
+    
+    struct IPP{
+        std::string ipv4;
+        std::vector<uint32_t> ports;
+    };
+      struct ipheader {
         uint8_t version_ihl;
         uint8_t DSField_ECN;
         uint16_t total_length;
@@ -28,14 +29,15 @@ typedef struct __attribute__((packed)){
         uint16_t headerChecksum;
         uint32_t sourceIP;
         uint32_t destIP;
-    } ipheader;
-typedef struct __attribute__((packed)){
+    } __attribute__((packed));
+
+     struct tcpheader{
         uint16_t sourcePort;
         uint16_t destinationPort;
         uint32_t sequenceNumber;
         uint32_t acknumber;
         struct {
-            unsigned headerLenght :4;
+            unsigned headerLength :4;
             unsigned reservedBits :6;
             unsigned URG :1;
             unsigned ACK :1;
@@ -43,33 +45,54 @@ typedef struct __attribute__((packed)){
             unsigned RST :1;
             unsigned SYN :1;
             unsigned FIN :1;
-        }flags;
+        } __attribute__((packed));
         uint16_t wSize;
         uint16_t checkSum;
         uint16_t urgentPointer;
     }tcpheader;
-
-}
-
-class IP{
-struct IPP{
-    std::string ipv4;
-    std::vector<uint32_t> ports;
+    inline static constexpr const char* IP_CONFIG_FILE = "data/tcpsyn_header.json";
+    inline static constexpr const char* IPFILE = "data/ip_addr.json";
+    
 };
 
 
-int readIpFile(){
-    WSADATA wsaData;
-    int iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
-    if (iResult != 0) {
-        std::cerr << "WSAStartup failed: " << iResult << "\n";
-        return 1;
+std::unique_ptr<IP::ipheader> ipHeader(IP::IPP* ipp){
+    nlohmann::json json;
+    std::ifstream file(IP::IP_CONFIG_FILE);
+    if(!file.is_open()){
+        std::cerr<<"IP_CONFIG_FILE ERR";
+        throw -1;
     }
+    file >>json; 
+    std::unique_ptr<IP::ipheader> p = std::make_unique<IP::ipheader>();
+    uint8_t version = json["version"];
+    uint8_t ihl = json["ihl"];
+    version &= 0x0F;
+    ihl &= 0xF;
+
+    //8bit
+    p->version_ihl = ((version<<4) | ihl);
+    p->DSField_ECN = json["DSField_ECN"];
+    //16bit
+    p->total_length = htons(json["total_length"]);
+    p->identification = htons(json["id"]);
+    p->flags_fragoff = htons(json["flags_fragoff"]);
+    //8bit
+    p->ttl = json["ttl"];
+    p->protocol = json["protocol"];
+    //32bit (already network byte order)
+    p->sourceIP = Packet::parseIPAddress(json["sourceIP"]);
+    p->destIP= Packet::parseIPAddress(json["destIP"][ip]);
+    std::cout <<"MEOW" <<p->destIP;
+    return p;
+}
+
+int readIpFile(){
 
     nlohmann::json json;
     std::ifstream file(IPFILE);
     if(!file.is_open()){
-        std::cerr<<"file failed";
+        std::cerr<<"IP_FILE ERR";
         throw -1;
     }
     file >> json;
@@ -87,9 +110,13 @@ int readIpFile(){
 }
 void packetcrafting(IPP* ipp){
 
+    std::unique_ptr<IP::ipheader> h= ipHeader(ipp);
+    //iphdr (reusable)
+    // loop for each port tcp headercrafting -> send ->new ->return
 
 }
-}
+
+
 int main(){
     try{
 
